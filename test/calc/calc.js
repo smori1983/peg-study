b93c3f9246a282d684f81d2b9e509934f1a0c9bb
smori <shinichiro.mori.1983@gmail.com>
2021-05-22 13:12:33 +0900

    [test] rewrite test for calc

--- test/calc/calc.js
+++ test/calc/calc.js
@@ -2,60 +2,40 @@ const {describe, it} = require('mocha');
 const assert = require('assert');
 const Calc = require('../../src/calc/calc');
 
+const calc = (input, params) => {
+  return new Calc().calc(input, params);
+};
+
 describe('calc', () => {
   describe('without placeholder - misc patterns', () => {
     it('1+2+3*4=15', () => {
-      const result = new Calc().calc('1+2+3*4');
-
-      assert.deepStrictEqual(result, 15);
+      assert.deepStrictEqual(calc('1+2+3*4'), 15);
     });
 
     it('(1)=1', () => {
-      const result = new Calc().calc('(1)');
-
-      assert.deepStrictEqual(result, 1);
+      assert.deepStrictEqual(calc('(1)'), 1);
     });
 
     it('(((1-1)))=0', () => {
-      const result = new Calc().calc('(((1-1)))');
-
-      assert.deepStrictEqual(result, 0);
+      assert.deepStrictEqual(calc('(((1-1)))'), 0);
     });
   })
 
   describe('use placeholder - additive', () => {
     it('integer + placeholder', () => {
-      const result = new Calc().calc('1 + $right', {
-        right: 3,
-      });
-
-      assert.deepStrictEqual(result, 4);
+      assert.deepStrictEqual(calc('1 + $right', {right: 3}), 4);
     });
 
     it('placeholder + integer', () => {
-      const result = new Calc().calc('$left + 3', {
-        left: 2,
-      });
-
-      assert.deepStrictEqual(result, 5);
+      assert.deepStrictEqual(calc('$left + 3', {left: 2}), 5);
     });
 
     it('placeholder + placeholder', () => {
-      const result = new Calc().calc('$value_1 + $value_2', {
-        value_1: 1,
-        value_2: 2,
-      });
-
-      assert.deepStrictEqual(result, 3);
+      assert.deepStrictEqual(calc('$value1 + $value2', {value1: 1, value2: 2}), 3);
     });
 
     it('placeholder - placeholder', () => {
-      const result = new Calc().calc('$left - $right', {
-        left: 1,
-        right: 2,
-      });
-
-      assert.deepStrictEqual(result, -1);
+      assert.deepStrictEqual(calc('$left - $right', {left: 1, right: 2}), -1);
     });
   });
 });
