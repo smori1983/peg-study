70cad4a9dca5c0a3f9aa970b67ac51366332a778
smori <shinichiro.mori.1983@gmail.com>
2021-05-24 01:30:41 +0900

    Initial implement of Scope and Evaluator

---
+++ src/loop/evaluator.js
@@ -0,0 +1,50 @@
+const Scope = require('./scope');
+
+class Evaluator {
+  /**
+   * @param {Object} ast
+   * @param {Scope} scope
+   */
+  evaluate(ast, scope) {
+    if (ast.type === 'language_construct' && ast.text === 'for') {
+      this._evaluateFor(ast, scope);
+    } else if (ast.type === 'language_construct' && ast.text === 'log') {
+      this._evaluateLog(ast, scope);
+    }
+  }
+
+  /**
+   * @param {Object} ast
+   * @param {Scope} parentScope
+   */
+  _evaluateFor(ast, parentScope) {
+    const arrayName = ast.array.text;
+    const variableName = ast.variable.text;
+
+    const array = parentScope.resolveVariable(arrayName);
+
+    if (!Array.isArray(array)) {
+      throw new Error(arrayName + ' is not array');
+    }
+
+    for (let i = 0; i < array.length; i++) {
+      const scope = new Scope(parentScope);
+      scope.addVariable(variableName, array[i]);
+
+      ast.children.forEach((child) => {
+        this.evaluate(child, scope);
+      });
+    }
+  }
+
+  /**
+   * @param {Object} ast
+   * @param {Scope} scope
+   */
+  _evaluateLog(ast, scope) {
+    const argName = ast.variable.text;
+    console.log(scope.resolveVariable(argName));
+  }
+}
+
+module.exports = Evaluator;
