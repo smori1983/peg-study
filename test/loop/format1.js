ad3c3db26b11df6b39136126dcb42ba3e0d3a10e
smori <shinichiro.mori.1983@gmail.com>
2021-05-26 23:50:07 +0900

    Test for format1.pegjs

---
+++ test/loop/format1.js
@@ -0,0 +1,101 @@
+const {describe, it} = require('mocha');
+const assert = require('assert');
+const parser = require('../../src/loop/format1');
+
+describe('loop - format1', () => {
+  describe('parse', () => {
+    it('pattern1', () => {
+      const input = [
+        'for(item in items) {',
+        '  log(item)',
+        '}',
+      ].join('\n');
+
+      const ast = {
+        "type": "root",
+        "text": "root",
+        "children": [
+          {
+            "type": "builtin",
+            "text": "for",
+            "array": {
+              "type": "variable",
+              "text": "items"
+            },
+            "variable": {
+              "type": "variable",
+              "text": "item"
+            },
+            "children": [
+              {
+                "type": "builtin",
+                "text": "log",
+                "variable": {
+                  "type": "variable",
+                  "text": "item"
+                }
+              }
+            ]
+          }
+        ]
+      };
+
+      assert.deepStrictEqual(parser.parse(input), ast);
+    });
+
+    it('pattern2', () => {
+      const input = [
+        'for(b in a) {',
+        '  for(c in b) {',
+        '    log(c)',
+        '  }',
+        '}',
+      ].join('\n');
+
+      const ast = {
+        "type": "root",
+        "text": "root",
+        "children": [
+          {
+            "type": "builtin",
+            "text": "for",
+            "array": {
+              "type": "variable",
+              "text": "a"
+            },
+            "variable": {
+              "type": "variable",
+              "text": "b"
+            },
+            "children": [
+              {
+                "type": "builtin",
+                "text": "for",
+                "array": {
+                  "type": "variable",
+                  "text": "b"
+                },
+                "variable": {
+                  "type": "variable",
+                  "text": "c"
+                },
+                "children": [
+                  {
+                    "type": "builtin",
+                    "text": "log",
+                    "variable": {
+                      "type": "variable",
+                      "text": "c"
+                    }
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      };
+
+      assert.deepStrictEqual(parser.parse(input), ast);
+    });
+  });
+});
