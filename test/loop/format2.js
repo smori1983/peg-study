3063543dbb85488dff3ebba59c76394de9b436e7
smori <shinichiro.mori.1983@gmail.com>
2021-05-27 00:02:18 +0900

    Test for format2.pegjs

---
+++ test/loop/format2.js
@@ -0,0 +1,90 @@
+const {describe, it} = require('mocha');
+const assert = require('assert');
+const parser = require('../../src/loop/format2');
+
+describe('loop - format2', () => {
+  describe('parse', () => {
+    it('pattern1', () => {
+      const input = [
+        'log(data.upper().lower())',
+        'for(part in data.split("-")) {',
+        '  log(part.upper())',
+        '}',
+      ].join('\n');
+
+      const ast = {
+        "type": "root",
+        "text": "root",
+        "children": [
+          {
+            "type": "builtin",
+            "text": "log",
+            "args": [
+              {
+                "type": "variable",
+                "text": "data",
+                "methods": [
+                  {
+                    "type": "method",
+                    "text": "upper",
+                    "args": []
+                  },
+                  {
+                    "type": "method",
+                    "text": "lower",
+                    "args": []
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "type": "builtin",
+            "text": "for",
+            "array": {
+              "type": "variable",
+              "text": "data",
+              "methods": [
+                {
+                  "type": "method",
+                  "text": "split",
+                  "args": [
+                    {
+                      "type": "string",
+                      "text": "-"
+                    }
+                  ]
+                }
+              ]
+            },
+            "variable": {
+              "type": "variable",
+              "text": "part"
+            },
+            "children": [
+              {
+                "type": "builtin",
+                "text": "log",
+                "args": [
+                  {
+                    "type": "variable",
+                    "text": "part",
+                    "methods": [
+                      {
+                        "type": "method",
+                        "text": "upper",
+                        "args": []
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      };
+
+      assert.deepStrictEqual(parser.parse(input), ast);
+    });
+  });
+});
