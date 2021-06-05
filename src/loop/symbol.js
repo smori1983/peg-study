82f8632c79717d09a53ebd49d1373b9c3b5eb523
smori <shinichiro.mori.1983@gmail.com>
2021-06-06 01:03:57 +0900

    Remove MethodInvoker

--- src/loop/symbol.js
+++ src/loop/symbol.js
@@ -1,16 +1,7 @@
-const MethodInvoker = require('./method-invoker');
 const Output = require('./output');
 const Scope = require('./scope');
 
 class Symbol {
-  constructor() {
-    /**
-     * @type {MethodInvoker}
-     * @protected
-     */
-    this._methodInvoker = new MethodInvoker();
-  }
-
   /**
    * @param {Scope} scope
    * @param {Output} output
