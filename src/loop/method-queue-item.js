1e3bd80af29bb09c259e340ca07bf6b9d83491a7
smori <shinichiro.mori.1983@gmail.com>
2021-06-06 00:33:38 +0900

    Copy check methods from MethodInvoker

--- src/loop/method-queue-item.js
+++ src/loop/method-queue-item.js
@@ -1,3 +1,4 @@
+const sprintf = require('sprintf-js').sprintf;
 const Method = require('./method');
 const MethodArg = require('./method-arg');
 const Scope = require('./scope');
@@ -19,6 +20,8 @@ class MethodQueueItem {
    * @return {*}
    */
   evaluate(scope, receiver) {
+    this._checkReceiverType(receiver);
+
     const args = [];
     this._args.forEach((arg) => {
       if (arg instanceof Variable2) {
@@ -27,7 +30,72 @@ class MethodQueueItem {
         args.push(arg);
       }
     });
-    return this._method.evaluate(receiver, args);
+
+    this._checkArguments(args);
+
+    const result = this._method.evaluate(receiver, args);
+
+    this._checkReturnValueType(result);
+
+    return result;
+  }
+
+  /**
+   * @param {*} receiver
+   * @throws {Error}
+   * @private
+   */
+  _checkReceiverType(receiver) {
+    const receiverType = this._getDataType(receiver);
+
+    if (receiverType !== this._method.getReceiverType()) {
+      throw new Error(sprintf('%s cannot use method %s', receiverType, this._method.getName()));
+    }
+  }
+
+  /**
+   * @param {MethodArg[]} args
+   * @throws {Error}
+   * @private
+   */
+  _checkArguments(args) {
+    if (args.length !== this._method.getArgTypes().length) {
+      throw new Error(sprintf('number of arguments of method %s should be %d', this._method.getName(), this._method.getArgTypes().length));
+    }
+
+    //for (let i = 0; i < args.length; i++) {
+    //  if (args[i].getType() !== this._method.getArgTypes()[i]) {
+    //    throw new Error(sprintf('argument type does not match for method %s', this._method.getName()));
+    //  }
+    //}
+  }
+
+  /**
+   * @param {*} returnValue
+   * @throws {Error}
+   * @private
+   */
+  _checkReturnValueType(returnValue) {
+    const returnValueType = this._getDataType(returnValue);
+
+    if (returnValueType !== this._method.getReturnType()) {
+      throw new Error(sprintf('return value of %s should be %s, actual was %s', this._method.getName(), this._method.getReturnType(), returnValueType));
+    }
+  }
+
+  /**
+   * @param {*} value
+   * @return {string}
+   * @private
+   */
+  _getDataType(value) {
+    if (value === null) {
+      return 'null';
+    } else if (Array.isArray(value)) {
+      return 'array';
+    } else {
+      return typeof value;
+    }
   }
 }
 
