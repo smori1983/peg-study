c8192265e663ff3066a4a22d0b8b84ffae14244b
smori <shinichiro.mori.1983@gmail.com>
2021-06-11 02:57:27 +0900

    Update test script

--- src/placeholder/format1-test1.js
+++ src/placeholder/format1-test1.js
@@ -1,37 +1,41 @@
 const parser = require('./format1');
 
-const input1 = '{value1}';
+/**
+ * @param {string} text
+ * @param {Object} option
+ */
+const dump = (text, option) => {
+  console.log(parser.parse(text.trim(), option));
+}
 
+const input1 = '{value1}';
 const option1 = {
   delimiter_open: '{',
   delimiter_close: '}',
 };
 
-console.log(parser.parse(input1.trim(), option1));
+dump(input1, option1);
 
 const input2 = '${value2}';
-
 const option2 = {
   delimiter_open: '${',
   delimiter_close: '}',
 };
 
-console.log(parser.parse(input2.trim(), option2));
+dump(input2, option2);
 
 const input3 = '{{value2}}';
-
 const option3 = {
   delimiter_open: '{{',
   delimiter_close: '}}',
 };
 
-console.log(parser.parse(input3.trim(), option3));
+dump(input3, option3);
 
-const input4 = '<value3>';
-
+const input4 = '[value3]';
 const option4 = {
-  delimiter_open: '<',
-  delimiter_close: '>',
+  delimiter_open: '[',
+  delimiter_close: ']',
 };
 
-console.log(parser.parse(input4.trim(), option4));
+dump(input4, option4);
