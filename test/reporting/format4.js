5cffbfdb6bd175bb2e4db11ad46d2f4aad69d56e
smori <shinichiro.mori.1983@gmail.com>
2021-06-25 01:20:57 +0900

    Add test

---
+++ test/reporting/format4.js
@@ -0,0 +1,44 @@
+const {describe, it} = require('mocha');
+const assert = require('assert');
+const ItemContainer = require('../../src/reporting/item-container');
+const Item = require('../../src/reporting/item');
+const Reporter = require('../../src/reporting4/format4-reporter');
+
+describe('reporting - format4', () => {
+  describe('output', () => {
+    it('pattern1', () => {
+      const itemContainer = new ItemContainer();
+      itemContainer.addItems([
+        new Item('100', 'item01', 100, ['xxx', 'yyy']),
+        new Item('200', 'item02', 200, ['zzz']),
+      ]);
+
+      const text = [
+        'report {',
+        '  code {',
+        '    100',
+        '    200',
+        '  }',
+        '  output {',
+        '    "# code:#{code} {#{name},#{amount}}"',
+        '    for (comment in comments) {',
+        '      "- #{comment}"',
+        '    }',
+        '  }',
+        '}',
+      ].join('\n');
+
+      const output = new Reporter().createReport(itemContainer, text);
+
+      const expected = [
+        '# code:100 {item01,100}',
+        '- xxx',
+        '- yyy',
+        '# code:200 {item02,200}',
+        '- zzz',
+      ];
+
+      assert.deepStrictEqual(output.getLines(), expected);
+    });
+  });
+});
