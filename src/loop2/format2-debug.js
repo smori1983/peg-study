65c9f40ca76750d6c24e066586e3c162385eb2ed
smori <shinichiro.mori.1983@gmail.com>
2021-06-26 03:59:36 +0900

    Add DebugBase

--- src/loop2/format2-debug.js
+++ src/loop2/format2-debug.js
@@ -1,24 +1,9 @@
 const parser = require('./format2');
-const Builder = require('../loop/builder');
-const Output = require('../loop/output');
-const Scope = require('../loop/scope');
+const DebugBase = require('../loop/debug-base');
 
-class Format2Debug {
-  /**
-   * @param {string} input
-   * @param {Scope} scope
-   * @returns {Output}
-   */
-  get(input, scope) {
-    const parsed = parser.parse(input);
-
-    const builder = new Builder();
-    const root = builder.build(parsed);
-    const output = new Output();
-
-    root.evaluate(scope, output);
-
-    return output;
+class Format2Debug extends DebugBase {
+  constructor() {
+    super(parser);
   }
 }
 
