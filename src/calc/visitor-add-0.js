d059d92fd663f4175d7fa9efe9930854302297d5
smori <shinichiro.mori.1983@gmail.com>
2022-11-17 08:51:47 +0900

    visitor-add-0.js

---
+++ src/calc/visitor-add-0.js
@@ -0,0 +1,35 @@
+/**
+ * Term rewriting
+ *
+ * Type: bottom up
+ *
+ * @param {Object} node
+ */
+const visit = (node) => {
+  if (node === null) {
+    return;
+  }
+
+  const left = node.children[0];
+  const right = node.children[1];
+
+  visit(left);
+  visit(right);
+
+  if (node.text === '+') {
+    if (left.text === 0 && right.text === 0) {
+      node.text = 0;
+      node.children = [null, null];
+    }
+    else if (left.text === 0) {
+      node.text = right.text;
+      node.children = right.children;
+    }
+    else if (right.text === 0) {
+      node.text = left.text;
+      node.children = left.children;
+    }
+  }
+};
+
+module.exports.visit = visit;
