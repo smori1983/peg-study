b449b5e21dc4b7eb79300819e86d1bc626ed230d
smori <shinichiro.mori.1983@gmail.com>
2022-12-04 14:10:57 +0900

    Initial implement of subtree-equivalent.js

---
+++ src/calc/subtree-equivalent.js
@@ -0,0 +1,77 @@
+/**
+ * @param {Object} node
+ */
+const execute = (node) => {
+  const list = {
+    '+': [],
+    '-': [],
+  };
+
+  foo(node, list);
+
+  return list;
+};
+
+const foo = (node, list) => {
+  if (node === null) {
+    return;
+  }
+
+  if (node.text === '+') {
+    add('+', node.children[0], list);
+    add('+', node.children[1], list);
+  }
+
+  if (node.text === '-') {
+    add('+', node.children[0], list);
+    add('-', node.children[1], list);
+  }
+};
+
+const add = (operator, node, list) => {
+  if (node.type === 'number') {
+    list[operator].push(node.text);
+  } else if (node.type === 'multi') {
+    const subList = executeMulti(node);
+    list[operator].push(subList);
+  } else {
+    foo(node, list);
+  }
+};
+
+const executeMulti = (node) => {
+  const list = {'*': [], '/': []};
+
+  fooMulti(node, list);
+
+  return list;
+};
+
+const fooMulti = (node, list) => {
+  if (node === null) {
+    return;
+  }
+
+  if (node.text === '*') {
+    multi('*', node.children[0], list);
+    multi('*', node.children[1], list);
+  }
+
+  if (node.text === '/') {
+    multi('*', node.children[0], list);
+    multi('/', node.children[1], list);
+  }
+};
+
+const multi = (operator, node, list) => {
+  if (node.type === 'number') {
+    list[operator].push(node.text);
+  } else if (node.type === 'multi') {
+    fooMulti(node, list);
+  } else {
+    const subList = execute(node);
+    list[operator].push(subList);
+  }
+};
+
+module.exports.execute = execute;
