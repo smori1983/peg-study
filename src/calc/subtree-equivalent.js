d8de33513fc76daac4b16ed9f7b971fc2ba2e521
smori <shinichiro.mori.1983@gmail.com>
2022-12-06 00:55:22 +0900

    subtree-equivalent.js track cumulative division count inside a group

--- src/calc/subtree-equivalent.js
+++ src/calc/subtree-equivalent.js
@@ -45,7 +45,7 @@ const add = (operator, node, list) => {
 const executeMulti = (node) => {
   const list = {'*': [], '/': []};
 
-  fooMulti(node, list);
+  fooMulti(node, list, 0);
 
   list['*'].sort();
   list['/'].sort();
@@ -53,27 +53,36 @@ const executeMulti = (node) => {
   return list;
 };
 
-const fooMulti = (node, list) => {
+const fooMulti = (node, list, divisionCount) => {
   if (node === null) {
     return;
   }
 
   if (node.text === '*') {
-    multi('*', node.children[0], list);
-    multi('*', node.children[1], list);
-  }
-
-  if (node.text === '/') {
-    multi('*', node.children[0], list);
-    multi('/', node.children[1], list);
+    if (divisionCount % 2 === 0) {
+      multi('*', node.children[0], list, divisionCount);
+      multi('*', node.children[1], list, divisionCount);
+    } else {
+      multi('/', node.children[0], list, divisionCount);
+      multi('/', node.children[1], list, divisionCount);
+    }
+  } else if (node.text === '/') {
+    divisionCount++;
+    if (divisionCount % 2 === 0) {
+      multi('/', node.children[0], list, divisionCount);
+      multi('*', node.children[1], list, divisionCount);
+    } else {
+      multi('*', node.children[0], list, divisionCount);
+      multi('/', node.children[1], list, divisionCount);
+    }
   }
 };
 
-const multi = (operator, node, list) => {
+const multi = (operator, node, list, divisionCount) => {
   if (node.type === 'number') {
     list[operator].push(node.text);
   } else if (node.type === 'multi') {
-    fooMulti(node, list);
+    fooMulti(node, list, divisionCount);
   } else {
     const subList = execute(node);
     list[operator].push(subList);
