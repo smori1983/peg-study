0a5c7983686ba9f2e5879c48980ad2eb4cdc3431
smori <shinichiro.mori.1983@gmail.com>
2022-12-17 00:12:50 +0900

    [test] bu-add-0

--- test/calc/calc2.js
+++ test/calc/calc2.js
@@ -3,6 +3,8 @@ const assert = require('assert');
 
 const parser = require('../../src/calc/calc2');
 const lispNotation = require('../../src/calc/helper/lisp-notation');
+const buAdd0 = require('../../src/calc/visitor/bu-add-0');
+const infixNotation = require('../../src/calc/helper/infix-notation');
 
 describe('calc2', () => {
   describe('lisp notation', () => {
@@ -14,6 +16,25 @@ describe('calc2', () => {
         const result = lispNotation.get(ast);
 
         assert.deepStrictEqual(result, output);
+      });
+    });
+  });
+
+  describe('visitor', () => {
+    describe('bu-add-0', () => {
+      const dataSet = require('./fixture/calc2-visitor').buAdd0;
+
+      dataSet.forEach(([input, output]) => {
+        it(`${input} = ${output}`, () => {
+          const ast1 = parser.parse(input);
+          buAdd0.visit(ast1);
+          const result1 = infixNotation.get(ast1);
+
+          const ast2 = parser.parse(output);
+          const result2 = infixNotation.get(ast2);
+
+          assert.deepStrictEqual(result1, result2);
+        });
       });
     });
   });
