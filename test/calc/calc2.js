a347d4f61bce3cfdecc802414125cced1fdd8343
smori <shinichiro.mori.1983@gmail.com>
2022-12-17 11:31:53 +0900

    [test] bu-multi-1

--- test/calc/calc2.js
+++ test/calc/calc2.js
@@ -5,6 +5,7 @@ const parser = require('../../src/calc/calc2');
 const lispNotation = require('../../src/calc/helper/lisp-notation');
 const buAdd0 = require('../../src/calc/visitor/bu-add-0');
 const buMulti0 = require('../../src/calc/visitor/bu-multi-0');
+const buMulti1 = require('../../src/calc/visitor/bu-multi-1');
 const infixNotation = require('../../src/calc/helper/infix-notation');
 
 describe('calc2', () => {
@@ -59,6 +60,23 @@ describe('calc2', () => {
         it(`${input} = ${output}`, () => {
           const ast1 = parser.parse(input);
           buMulti0.visit(ast1);
+          const result1 = infixNotation.get(ast1);
+
+          const ast2 = parser.parse(output);
+          const result2 = infixNotation.get(ast2);
+
+          assert.deepStrictEqual(result1, result2);
+        });
+      });
+    });
+
+    describe('bu-multi-1', () => {
+      const dataSet = require('./fixture/calc2-visitor').buMulti1;
+
+      dataSet.forEach(([input, output]) => {
+        it(`${input} = ${output}`, () => {
+          const ast1 = parser.parse(input);
+          buMulti1.visit(ast1);
           const result1 = infixNotation.get(ast1);
 
           const ast2 = parser.parse(output);
