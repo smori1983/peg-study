272bef8970e69ea7114fb4ce04fdf19d69de2fe9
smori <shinichiro.mori.1983@gmail.com>
2023-01-09 13:45:37 +0900

    Remove lip

--- src/lip/README.md
+++
@@ -1,3 +0,0 @@
-Implementing 'Language Implementation Patterns'.
-
-https://www.oreilly.co.jp/books/9784873115320/
--- src/lip/graphics-run.js
+++
@@ -1,9 +0,0 @@
-const parser = require('./graphics');
-
-const input = `
-line from 1,1 to 2,2
-line from 10,0 to 0,10
-line from 0,10 to 10,10
-`;
-
-console.log(parser.parse(input.trim()));
