84e66fa410cb9be6491b0815cee62d4fc0df0e8d
smori <shinichiro.mori.1983@gmail.com>
2023-01-20 02:12:12 +0900

    [test] add input pattern

--- test/condition/_data/block.js
+++ test/condition/_data/block.js
@@ -46,4 +46,43 @@ module.exports = [
       '2',
     ],
   },
+  {
+    input: `
+      if (a + 1 > 10) {
+        log('1')
+      }
+      if (a + b / 2 < c) {
+        log('2')
+      }
+      if ((a + b) * 10 == a * c) {
+        log('3')
+      }
+
+      log('---')
+
+      if (a + b >= 10) {
+        if (c < 30) {
+          log('4')
+        } elseif (c == 30) {
+          log('5')
+        } else {
+          log('6')
+        }
+      } else {
+        log('4')
+      }
+    `,
+    variables: {
+      a: 10,
+      b: 20,
+      c: 30,
+    },
+    output: [
+      '1',
+      '2',
+      '3',
+      '---',
+      '5',
+    ],
+  },
 ];
